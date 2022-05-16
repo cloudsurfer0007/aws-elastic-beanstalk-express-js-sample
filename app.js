37260e927a1bef0eb5fa616873dd8c4d28bf85e6
cloudsurfer0007 <99864801+cloudsurfer0007@users.noreply.github.com>
2022-05-16 13:35:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! I am Sarthak abd this is my code after changing and creating my first AWS Pipeline.'));
+app.get('/', (req, res) => res.send('Hello World! I am Sarthak abd this is my code after changing and creating my first AWS Pipeline using AWS Educate.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
